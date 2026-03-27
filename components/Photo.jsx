7a467109528eae1b974c94cd0b75c52e8ff8b7b5
DevJawad1 <adekanbija1@gmail.com>
2026-03-27 09:33:36 +0100

    updating portfolio

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,7 @@ const Photo = () => {
           }} 
           className='w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute'>
           <Image
-            src="/assets/portrait.png"
+            src="/assets/portrait4.png"
             priority
             quality={100}
             fill
